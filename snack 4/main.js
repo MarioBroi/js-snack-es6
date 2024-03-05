be3a8b3596e60cc14f7f56610475609067717663
MarioBroi <inoix16@gmail.com>
2024-03-05 12:56:12 +0000

    add snack 4

---
+++ snack 4/main.js
@@ -0,0 +1,16 @@
+console.log('test');
+
+//- Crea un array di oggetti che rappresentano delle persone.
+const persone = [
+    //- Ogni persona ha un nome, un cognome e un’età.
+    { nome: 'Franco', cognome: 'Mazzuoli', eta: 45 },
+    { nome: 'Ignazio', cognome: 'Cocco', eta: 17 },
+    { nome: 'Basilio', cognome: 'Dettori', eta: 32 },
+    { nome: 'Stefano', cognome: 'Lai', eta: 21 },
+    { nome: 'Giovanni', cognome: 'Sirigu', eta: 19 },
+    { nome: 'Giuseppe', cognome: 'Strazzera', eta: 12 },
+    { nome: 'Dario', cognome: 'Caria', eta: 68 },
+];
+
+//- Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+//- Confermate lettura come al solito e buon divertimento
