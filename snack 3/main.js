48a5f0dc3477ed834ba8cbae09d908b56c564283
MarioBroi <inoix16@gmail.com>
2024-03-05 12:42:59 +0000

    first commit

---
+++ snack 3/main.js
@@ -0,0 +1,15 @@
+console.log('test');
+
+//- Crea un array di oggetti che rappresentano degli animali.
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+    { nome: 'capra', famiglia: 'bovidi', classe: 'mammiferi' },
+    { nome: 'pappagallo', famiglia: 'psittacidae', classe: 'uccelli' },
+    { nome: 'rana', famiglia: 'ranide', classe: 'anfibi' },
+    { nome: 'orso', famiglia: 'ursidae', classe: 'mammiferi' },
+    { nome: 'coccodrillo', famiglia: 'crocody;idae', classe: 'rettili' },
+    { nome: 'puma', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'acquila', famiglia: 'accipitridae', classe: 'uccelli' },
+];
