f2f3640e4c0bf2ec2fca6d58eb43ae8f8b619f0a
MarioBroi <inoix16@gmail.com>
2024-03-05 12:48:25 +0000

    add script to filter mammiferi

--- snack 3/main.js
+++ snack 3/main.js
@@ -2,6 +2,7 @@ console.log('test');
 
 //- Crea un array di oggetti che rappresentano degli animali.
 const animals = [
+    //- Ogni animale ha un nome, una famiglia e una classe.
     { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
     { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
     { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
@@ -13,3 +14,8 @@ const animals = [
     { nome: 'puma', famiglia: 'felidi', classe: 'mammiferi' },
     { nome: 'acquila', famiglia: 'accipitridae', classe: 'uccelli' },
 ];
+
+//- Crea un nuovo array con la lista dei mammiferi.
+
+let mammiferi = animals.filter(animals => animals.classe == 'mammiferi')
+console.log(mammiferi);
